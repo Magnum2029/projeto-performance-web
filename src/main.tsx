02a1e9c0e4c7aa15ae4ec8216274da5853a5a662
Magnum2029 <magnumsantos97@gmail.com>
2026-09-02 12:40:56 +0100

    Corrige caminho base do React Router

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio-magnum-react/">
+    <BrowserRouter basename="/projeto-performance-web/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
